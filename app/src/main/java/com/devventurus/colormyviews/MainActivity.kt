7a88ecd604316e01606bd9b202024925b8bf1fb6
WesleyFirmino <wesleydasilva_firmino@live.com>
2021-06-24 15:37:49 -0300

    Changed button - Setting for check permission and sharing images

--- app/src/main/java/com/devventurus/colormyviews/MainActivity.kt
+++ app/src/main/java/com/devventurus/colormyviews/MainActivity.kt
@@ -1,31 +1,41 @@
 package com.devventurus.colormyviews
 
-import android.content.Context
+import android.Manifest
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
     var boxes = arrayOf( R.id.boxOne, R.id.boxTwo, R.id.boxThree, R.id.boxFour, R.id.boxFive )
     var colorLocate: Int = R.color.gray
     lateinit var sharedPreferences : SharedPreferences
-    val shareButton: FloatingActionButton? by lazy { findViewById(R.id.shareButton) }
+    val shareButton: FloatingActionButton? by lazy { findViewById(R.id.share) }
+    private val SOLICITAR_PERMISSAO = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sharedPreferences = getSharedPreferences("colors", Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences("colors", MODE_PRIVATE)
         for (box in boxes) {
             findViewById<TextView>(box).setBackgroundResource(sharedPreferences.getInt("$box", colorLocate))
         }
     }
+
+
 
     fun onButtonClick(view: View) {
         colorLocate = when (view.id) {
@@ -46,16 +56,60 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Return a bitmap
-    fun takeScreenshot(view: View, height: Int, width: Int): Bitmap {
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
+    fun takeScreenshot(view: View) {
+
+        //Defina um bitmap com o mesmo tamanho da visualização
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+
+        //Vincule uma tela a ele
+        val canvas = Canvas(returnedBitmap)
+
+        //Obtenha o background
         val bgDrawable = view.background
+
         if (bgDrawable != null) {
-            bgDrawable.draw(canvas)
+            bgDrawable.draw(canvas) //Obtenha o background em drawable, then draw it on the canvas
         } else {
-            canvas.drawColor(Color.WHITE)
+            canvas.drawColor(Color.WHITE) //Se caso o não tiver o background em drawable a imagem será impressa em branco
         }
+
         view.draw(canvas)
-        return bitmap
+
+        //chama a permissao
+        checkSharedPermission(returnedBitmap)
     }
+
+    //Valida verificação
+    fun checkSharedPermission(bitmap: Bitmap) {
+
+        // Verifica  o estado da permissão de WRITE_EXTERNAL_STORAGE
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            // Se for diferente de PERMISSION_GRANTED, então vamos exibir a tela padrão
+            ActivityCompat.requestPermissions( this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), SOLICITAR_PERMISSAO )
+        } else {
+            // Senão vamos compartilhar a imagem
+            shareImage(bitmap)
+        }
+    }
+
+    //Compartilha o bitmap
+    fun shareImage(bitmap: Bitmap) {
+
+        val share = Intent(Intent.ACTION_SEND)
+        share.type = "image/jpeg"
+        // Gravamos a imagem
+        val path = MediaStore.Images.Media.insertImage(contentResolver, bitmap, "Obra de Arte", null)
+
+        // criamos uam Uri com o endereço que a imagem foi salva
+        val imageUri: Uri = Uri.parse(path)
+
+        // Setmaos a Uri da imagem
+        share.putExtra(Intent.EXTRA_STREAM, imageUri)
+
+        // chama o compartilhamento
+        startActivity(Intent.createChooser(share, "Selecione"))
+    }
+
+
 }
